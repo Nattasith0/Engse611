d89e35c85e1db5025af5ad8c457a4a2d76008467
Nattasith0 <gunkung.x20@gmail.com>
2025-01-28 10:49:04 +0700

    engse611

---
+++ lab5/01-variables-data-types/03-variables/script.js
@@ -0,0 +1,47 @@
+let name;
+let surname;
+
+let Surname;
+let cats;
+
+let catS=["Cherry", "Joss"];
+
+
+let schoolname;
+let school_name;
+let schoolName;
+
+my_name = "Nattasith"; //Asignment Statement
+
+console.log(my_name);
+let a=1;
+let b=2;
+let c=1.3;
+
+console.log(a, typeof a );
+console.log(b, typeof b );
+console.log(c, typeof c );
+
+let bigNumber=1000000n;
+console.log(bigNumber, typeof bigNumber );
+
+console.log(cats, typeof cats );
+
+console.log(my_name, typeof my_name );
+
+let is_deleted = false;
+
+console.log(is_deleted, typeof is_deleted );
+
+a=1;
+console.log(a, typeof a );
+
+a="Test"; //re-assign to string value
+console.log(a, typeof a );
+a = a+1;
+console.log(a, typeof a );
+
+b=2;
+console.log(b, typeof b );
+
+
